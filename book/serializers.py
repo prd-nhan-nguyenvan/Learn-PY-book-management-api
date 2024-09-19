fc2ef0d71635a9a9e48a615dc118313bbfc9c2a3
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-09-19 14:10:49 +0700

    change to hyperlink serializer

--- book/serializers.py
+++ book/serializers.py
@@ -4,23 +4,29 @@ from rest_framework.validators import UniqueTogetherValidator
 
 from .models import Book, Author
 
-class AuthorSerializer(serializers.ModelSerializer):
+class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+    books  = serializers.HyperlinkedRelatedField(many=True, view_name='book-detail', read_only=True)
+
+
     class Meta:
         model = Author
-        fields = ['id', 'name', 'birth_date', 'nationality']
+        fields = ['id', 'url', 'name', 'birth_date', 'nationality', 'books']
 
-class BookSerializer(serializers.ModelSerializer):
-    author = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all())  # Reference existing authors by ID
-
+class BookSerializer(serializers.HyperlinkedModelSerializer):
+    author_name = serializers.SerializerMethodField()
+    author_url = serializers.HyperlinkedIdentityField(view_name='author-detail')  # Reference existing authors by ID
     class Meta:
         model = Book
-        fields = ['author', 'language', 'link', 'pages', 'title', 'publication_date']
+        fields = ['url', 'id','author_name', 'language', 'link', 'pages', 'title', 'publication_date', 'author_url']
         validators = [
             UniqueTogetherValidator(
                 queryset=Book.objects.all(),
                 fields=['author', 'title']
             )
         ]
+
+    def get_author_name(self, obj):
+        return obj.author.name  # Assuming the Author model has a 'name' field
 
     # Custom validation for the publication_date field
     def validate_publication_date(self, value):
