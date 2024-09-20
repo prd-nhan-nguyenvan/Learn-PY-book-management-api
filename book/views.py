ec4875ea049dd5e73e6f3906acdd6d93763f655f
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-09-20 10:04:39 +0700

    chore: run black and isort hook in precommit

--- book/views.py
+++ book/views.py
@@ -1,29 +1,36 @@
-from django.core.serializers import serialize
-from django.template.context_processors import request
-from django.template.defaulttags import querystring
-from rest_framework import generics, status, viewsets, permissions
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework import status, viewsets
+from rest_framework.authentication import (
+    BasicAuthentication,
+    SessionAuthentication,
+    TokenAuthentication,
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from book.models import Book, Author
-from book.serializers import BookSerializer, AuthorSerializer
+from book.models import Author, Book
+from book.serializers import AuthorSerializer, BookSerializer
+
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    authentication_classes = [TokenAuthentication,SessionAuthentication, BasicAuthentication]
+    authentication_classes = [
+        TokenAuthentication,
+        SessionAuthentication,
+        BasicAuthentication,
+    ]
     permission_classes = [IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
         data = request.data
 
         if isinstance(data, list):
-            serializer = BookSerializer(data=data, many=True, context={'request' : request})
+            serializer = BookSerializer(
+                data=data, many=True, context={"request": request}
+            )
         else:
-            serializer = BookSerializer(data=data, context={'request' : request})
+            serializer = BookSerializer(data=data, context={"request": request})
 
         # Validate and save
         if serializer.is_valid():
@@ -31,28 +38,35 @@ class BookViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class BookListLanguage(APIView):
 
     def get(self, request, language):
         query_set = Book.objects.all().filter(language=language)
-        serializer = BookSerializer(query_set, many=True,context={'request': request})
+        serializer = BookSerializer(query_set, many=True, context={"request": request})
         return Response(serializer.data)
+
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-    authentication_classes = [TokenAuthentication, SessionAuthentication, BasicAuthentication]
+    authentication_classes = [
+        TokenAuthentication,
+        SessionAuthentication,
+        BasicAuthentication,
+    ]
     permission_classes = [IsAuthenticated]
 
-
     def create(self, request, *args, **kwargs):
-        '''Custom for add multi authors'''
+        """Custom for add multi authors"""
         data = request.data
 
         if isinstance(data, list):
-            serializer = AuthorSerializer(data=data, many=True, context={'request': request})
+            serializer = AuthorSerializer(
+                data=data, many=True, context={"request": request}
+            )
         else:
-            serializer = AuthorSerializer(data=data, context={'request': request})
+            serializer = AuthorSerializer(data=data, context={"request": request})
 
         # Validate and save
         if serializer.is_valid():
--- users/views.py
+++ users/views.py
@@ -1,47 +1,54 @@
-from django.contrib.auth import authenticate, login as django_login
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as django_login
+from django.contrib.auth.models import User
+from rest_framework import status
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import status
-
-from django.shortcuts import get_object_or_404
-from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
 
 from .serializers import UserSerializer
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 def signup(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        user = User.objects.get(username=request.data['username'])
-        user.set_password(request.data['password'])
+        user = User.objects.get(username=request.data["username"])
+        user.set_password(request.data["password"])
         user.save()
         token = Token.objects.create(user=user)
-        return Response({'token': token.key, 'user': serializer.data})
+        return Response({"token": token.key, "user": serializer.data})
     return Response(serializer.errors, status=status.HTTP_200_OK)
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def login(request):
-    username = request.data.get('username')
-    password = request.data.get('password')
+    username = request.data.get("username")
+    password = request.data.get("password")
 
     user = authenticate(username=username, password=password)
 
     if user is None:
-        return Response("Invalid username or password", status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            "Invalid username or password", status=status.HTTP_400_BAD_REQUEST
+        )
 
     django_login(request, user)
 
     token, created = Token.objects.get_or_create(user=user)
 
     serializer = UserSerializer(user)
-    return Response({'token': token.key, 'user': serializer.data})
+    return Response({"token": token.key, "user": serializer.data})
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def test_token(request):
